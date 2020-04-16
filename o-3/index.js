fa4e3906ed561ea74cb30ccad0856299595ba71c
amaeri <amaericsson@gmail.com>
2020-04-16 23:22:22 +0200

    Small changes

--- o-3/index.js
+++ o-3/index.js
@@ -1,12 +1,8 @@
-const bassoon = document.querySelector("#bassoon");
-const flute = document.querySelector("#flute");
-const clarinet = document.querySelector("#clarinet");
-
 const btnB = document.querySelector("#btnB");
 const btnF = document.querySelector("#btnF");
 const btnC = document.querySelector("#btnC");
 
-function playBassoon(evt) {
+function playBassoon() {
     bassoon.play(); 
 }
 
